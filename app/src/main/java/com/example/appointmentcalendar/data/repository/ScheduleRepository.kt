bf941c2566c588576d67d0f9566b94f23372eb25
Ryder Chen <ryder.0064@gmail.com>
2022-05-19 01:37:37 +0800

    handle time

--- app/src/main/java/com/example/appointmentcalendar/data/repository/ScheduleRepository.kt
+++ app/src/main/java/com/example/appointmentcalendar/data/repository/ScheduleRepository.kt
@@ -10,6 +10,9 @@ import com.example.appointmentcalendar.util.TAG
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.IOException
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ScheduleRepository(
     private val scheduleService: ScheduleService,
@@ -30,7 +33,7 @@ class ScheduleRepository(
         withContext(Dispatchers.IO) {
             try {
                 //TODO getFirstDayTimeOfWeek
-                fetchTeacherSchedule("2022-05-22T16:00:00.000Z")
+                fetchTeacherSchedule(getFirstDayTimeOfWeek())
             } catch (exception: Exception) {
                 when (exception) {
                     is IOException -> snackBarMessage.postValue("Network problem occurred")
@@ -59,5 +62,30 @@ class ScheduleRepository(
 
     private fun saveTeacherSchedule(data: TeacherSchedule) {
         teacherScheduleDao.insertTeacherSchedule(data)
+    }
+
+    private fun getFirstDayTimeOfWeek(): String {
+         val c = Calendar.getInstance(TimeZone.getDefault())
+        c[Calendar.DAY_OF_WEEK] = c.firstDayOfWeek
+        c[Calendar.HOUR_OF_DAY] = 0
+        c[Calendar.MINUTE] = 0
+        c[Calendar.SECOND] = 0
+        c[Calendar.MILLISECOND] = 0
+        Log.d(TAG, "QQ: ${SimpleDateFormat("yyyy-M-dd-HH:mm:ss", Locale.getDefault()).format(c.time)}")
+        val df: DateFormat = SimpleDateFormat("yyyy-M-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+        df.timeZone = TimeZone.getTimeZone("UTC");
+        Log.d(TAG, "QQ: ${df.format(c.time)}")
+
+        getTimeWithTimeZone(df.format(c.time))
+        return df.format(c.time)
+    }
+
+    private fun getTimeWithTimeZone(time:String){
+        var df: DateFormat = SimpleDateFormat("yyyy-M-dd'T'hh:mm:ss.SSS'Z'", Locale.getDefault())
+        df.timeZone = TimeZone.getTimeZone("UTC")
+        val date = df.parse(time)!!
+        df = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.getDefault())
+        df.setTimeZone(TimeZone.getDefault())
+        Log.d(TAG, "QQ: ${df.format(date)}")
     }
 }
